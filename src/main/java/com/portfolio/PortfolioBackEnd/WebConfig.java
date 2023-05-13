b6cbcb3d877e275aba0d6cd8f9c2904edcba026d
Lucas Flores <lucasfloores164example.com>
2023-05-13 06:14:19 +0100

    userClass ready

--- src/main/java/com/portfolio/PortfolioBackEnd/WebConfig.java
+++ src/main/java/com/portfolio/PortfolioBackEnd/WebConfig.java
@@ -1,4 +1,4 @@
-/*package com.portfolio.PortfolioBackEnd;
+package com.portfolio.PortfolioBackEnd;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,4 +15,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
 
     }
-}*/
+}
